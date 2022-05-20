31969639880da87a26cbf29eb78728b4672f0745
casibanryan <restyochea@gmail.com>
2022-05-20 13:31:26 +0800

    update

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // setting to make ejs as view engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-// routes 
-app.get('/', (req, res) => {
-  res.render("index.ejs")
-})
+// routes
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get('/new-client', (req, res) => {
-  res.render("new_client.ejs")
-})
+app.get("/new-client", (req, res) => {
+  res.render("new_client.ejs");
+});
 
 app.get("/manage-clients", (reg, res) => {
-  res.render("manage_clients.ejs")
-})
+  res.render("manage_clients.ejs");
+});
+
+app.get("/client-details", (req, res) => {
+  res.render("client_details.ejs");
+});
 
 /* Telling the server to use the public folder as the static folder. */
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const port = 8080
+const port = 8080;
 
 /* Telling the server to listen on port 8080. */
 app.listen(port, () => {
-  console.log(`Server listening on port http:localhost:${port}`)
-})
+  console.log(`Server listening on port http:localhost:${port}`);
+});
